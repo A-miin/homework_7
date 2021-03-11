dd5f95c0e1ee3dd0bf1e10d4ac8781ed2c7e6598
A-miin <zulaykaisaeva@gmail.com>
2021-03-11 14:15:06 +0600

    Register models in admin

--- issue_tracker/tracker/admin.py
+++ issue_tracker/tracker/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Type,Status, Issue
 
 # Register your models here.
+
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    fields = ['id', 'name']
+
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    fields = ['id', 'name']
+
+class IssueAdmin(admin.ModelAdmin):
+    list_display = ['id', 'summary', 'status','created_at','updated_at']
+    fields = ['id', 'summary', 'description', 'type', 'status', 'created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at']
+
+admin.site.register(Issue, IssueAdmin)
+admin.site.register(Type, TypeAdmin)
+admin.site.register(Status, StatusAdmin)
