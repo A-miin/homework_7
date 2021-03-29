0f646c4676400b017bdf1eaccfc80b92ccf1cea4
A-miin <zulaykaisaeva@gmail.com>
2021-03-29 15:08:17 +0600

    Add Project, add migrations

--- issue_tracker/tracker/models.py
+++ issue_tracker/tracker/models.py
@@ -24,12 +24,12 @@ class Status(models.Model):
     def __str__(self):
         return self.name
 
-
 class Issue(models.Model):
     summary = models.CharField(max_length=120, null=False, blank=False, verbose_name='Краткое описание',validators=[validate_string, validate_words, MinLengthValidator(3)])
     description = models.TextField(max_length=1024,null=True, blank=True, verbose_name='Полное описание', validators=[validate_words_count, validate_words])
     status = models.ForeignKey('tracker.Status', on_delete=models.PROTECT, verbose_name='Статус')
     type = models.ManyToManyField('tracker.Type', related_name="issues", verbose_name='Тип', blank=True)
+    project = models.ForeignKey('tracker.Project', on_delete=models.CASCADE, related_name='issues', verbose_name="Проект")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now = True)
 
@@ -40,3 +40,18 @@ class Issue(models.Model):
 
     def __str__(self):
         return f'{self.id}. {self.summary}: {self.type}'
+
+
+class Project(models.Model):
+    begin_date = models.DateField(verbose_name="Дата начала")
+    end_date = models.DateField(verbose_name="Дата конца", null=True, blank=False)
+    name = models.CharField(max_length=120, verbose_name="Название")
+    description = models.TextField(max_length=3072, verbose_name="Описание")
+
+    class Meta:
+        verbose_name="Проект"
+        verbose_name_plural = "Проекты"
+
+    def __str__(self):
+        return f'{self.name}: {self.begin_date}-{self.end_date}'
+
