c6bb657a16fa0bbd64f8368da97bf30893404ccb
A-miin <zulaykaisaeva@gmail.com>
2021-04-06 20:50:48 +0600

    Add django.Login Logout

--- issue_tracker/accounts/urls.py
+++ issue_tracker/accounts/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from accounts.views import login_view, logout_view
+# from accounts.views import login_view, logout_view
 
 
 urlpatterns=[
-    path('login', login_view, name='login' ),
-    path('logout', logout_view, name='logout' ),
+    path('login', LoginView.as_view(), name='login' ),
+    path('logout', LogoutView.as_view(), name='logout' ),
 
 ]
