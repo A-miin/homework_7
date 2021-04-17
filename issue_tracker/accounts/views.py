eacc694a65116477c1ea9b15a9cb35007f0287e7
A-miin <zulaykaisaeva@gmail.com>
2021-04-17 17:30:57 +0600

    Add user detail view

--- issue_tracker/accounts/views.py
+++ issue_tracker/accounts/views.py
@@ -1,28 +1,12 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator
+
+from django.views.generic import DetailView
 
 from accounts.forms import UserRegisterForm
-
-# Create your views here.
-# def login_view(request, *args, **kwargs):
-#     context = {}
-#     if request.method == 'POST':
-#         username = request.POST.get('username')
-#         password = request.POST.get('password')
-#
-#         user = authenticate(request, username=username, password=password)
-#         if user is not None:
-#             login(request, user)
-#             return redirect('tracker:project-list')
-#         else:
-#             context['has_error']=True
-#     return render(request, 'registration/login.html', context=context)
-#
-# def logout_view(request, *args, **kwargs):
-#     logout(request)
-#     return redirect('tracker:issue-list')
-
 
 def register_view(request, *args, **kwargs):
     if request.method=='POST':
@@ -34,3 +18,21 @@ def register_view(request, *args, **kwargs):
     else:
         form = UserRegisterForm()
     return render(request, 'registration/create.html', context={'form':form})
+
+class UserDetailView(LoginRequiredMixin, DetailView):
+    model = get_user_model()
+    template_name = 'user_view.html'
+    context_object_name = 'user_object'
+    paginate_related_by = 5
+    paginate_related_orphans = 0
+
+    def get_context_data(self, **kwargs):
+        projects = self.object.projects.filter(is_deleted=False)
+        paginator = Paginator(projects, self.paginate_related_by, orphans=self.paginate_related_orphans)
+        page_number = self.request.GET.get('page', 1)
+        page = paginator.get_page(page_number)
+        kwargs['page_obj'] = page
+        kwargs['projects'] = page.object_list
+        kwargs['is_paginated']=page.has_other_pages()
+        return super().get_context_data(**kwargs)
+
--- issue_tracker/tracker/models.py
+++ issue_tracker/tracker/models.py
@@ -61,7 +61,7 @@ class Profile(models.Model):
     user = models.OneToOneField(get_user_model(), related_name='profile', on_delete=models.CASCADE,
                                 verbose_name="Пользователь")
     avatar = models.ImageField(null=True, blank=True, upload_to='avatars', verbose_name="Аватар")
-    githubLink = models.URLField(max_length=512, verbose_name="Ссылка на гитхаб")
+    githubLink = models.URLField(null=True, blank=True, max_length=512, verbose_name="Ссылка на гитхаб")
     description = models.TextField(max_length=2048, null=True, blank=True, verbose_name="О себе")
 
     class Meta:
