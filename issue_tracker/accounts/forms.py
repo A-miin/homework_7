1777e0da0ef0d2244c142fd427cd8fff9d871226
A-miin <zulaykaisaeva@gmail.com>
2021-04-20 20:42:49 +0600

    Add Update Password form

--- issue_tracker/accounts/forms.py
+++ issue_tracker/accounts/forms.py
@@ -27,3 +27,32 @@ class ProfileChangeForm(forms.ModelForm):
         model = Profile
         exclude=['user']
 
+class PasswordUpdateForm(forms.ModelForm):
+    password = forms.CharField(label="New password", strip=False, widget=forms.PasswordInput)
+    password_confirm = forms.CharField(label="Confirm password", strip=False, widget=forms.PasswordInput)
+    old_password = forms.CharField(label="Old password", strip=False, widget=forms.PasswordInput)
+    def clean_password_confirm(self):
+        password = self.cleaned_data.get('password')
+        password_confirm = self.cleaned_data.get('password_confirm')
+        if password_confirm and password and (password!=password_confirm):
+            raise forms.ValidationError('Password confirm error!')
+        return password_confirm
+
+    def clean_old_password(self):
+        old_password = self.cleaned_data.get('old_password')
+        if not self.instance.check_password(old_password):
+            raise forms.ValidationError('Old password error')
+        return old_password
+
+    def save(self, commit = True):
+        user = self.instance
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+        return user
+
+    class Meta:
+        model = get_user_model()
+        fields = ['password', 'password_confirm', 'old_password']
+
+
